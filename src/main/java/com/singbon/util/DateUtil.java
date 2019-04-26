c69b50d6e1eabb13167209fd8968bb3b044e032a
zchcpy <zghnzch@163.com>
2019-04-26 16:23:59 +0800

    add ignore

--- src/main/java/com/singbon/util/DateUtil.java
+++ src/main/java/com/singbon/util/DateUtil.java
@@ -1,5 +1,16 @@
 package com.singbon.util;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 /**
  * @author zch
  */
-public class DateUtil {}
+public class DateUtil {
+	/**
+	 获取当前系统时间
+	 */
+	public static String getNowTime(){
+		// 设置日期格式
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+		return df.format(new Date());
+	}
+}
